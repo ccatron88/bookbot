8d9f7669e73b7611659cffd1ece6076e1ee8b7ad
ccatron88 <christophercatron@outlook.com>
2025-08-25 23:21:25 -0500

    finished BOOKBOT project

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+from stats import print_report  
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        num_words, char_list = print_report(sys.argv[1])
+
+        print(
+            f'''
+            ============ BOOKBOT ============
+            Analyzing book found at books/frankenstein.txt...
+            ----------- Word Count-----------
+            Found {num_words} total words
+            -------- Character Counts -------
+            {char_list}
+            ============= END ===============
+        ''')
+
+main()
---
+++ stats.py
@@ -0,0 +1,35 @@
+def get_num_words(filepath):
+    with open(filepath) as b:
+        character_counts = {}
+        book_text = b.read()
+        book_data = book_text.lower().split()
+        word_count = len(book_data)
+
+        for word in book_data:
+            for character in word:
+                if character not in character_counts:
+                    character_counts[character] = 1
+                else:
+                    character_counts[character] += 1
+    return word_count, character_counts
+
+def print_report(filepath):
+    num_words, char_list = get_num_words(filepath)
+    counts_dict = []
+    counts_list = []
+
+    for char in char_list:
+        if char.isalpha():
+            char_dict = {'char': char, 'count': char_list[char]}
+            counts_dict.append(char_dict)
+
+    def sort_on(items):
+        return items["count"]
+    
+    counts_dict.sort(reverse=True, key=sort_on)
+
+    for item in counts_dict:
+        counts_list.append(f"{item['char']}: {item['count']}")
+
+    return num_words, counts_list
+        
